f69bd40606350a31bdbe55f3b5e74d75c8b4d794
Nailer <ajeemmanuel221@gmail.com>
2024-07-26 20:19:11 +0100

    removed the private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const PRIVATE_KEY = '9a4a2ed4f71965ac692b0e9f456135c1d569be4fd010d637c551d8b82708b47b'
+const PRIVATE_KEY = '9a4a2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb47b'
 module.exports = {
   defaultNetwork: "swisstronik",
   solidity: "0.8.24",
