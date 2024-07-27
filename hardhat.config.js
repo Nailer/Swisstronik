df43f66f9379281a4ffd8160d95e08c5d53e7993
Nailer <ajeemmanuel221@gmail.com>
2024-07-27 06:30:17 +0100

    completed the smart contract cofiguration

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const PRIVATE_KEY = '9a4a2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb47b'
+const PRIVATE_KEY = '9a4a2xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb47b'  // use the private key as a const due to security of my wallet on the terminal
 module.exports = {
   defaultNetwork: "swisstronik",
   solidity: "0.8.24",
